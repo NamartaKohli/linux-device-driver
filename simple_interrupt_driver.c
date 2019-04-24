4d82f928246922ed3a85e7384370710f21a0a85d
NamartaKohli <49906486+NamartaKohli@users.noreply.github.com>
2019-04-24 07:25:08 +0200

    Device driver for Interrupt handling
    Interrupt handling in linux

---
+++ simple_interrupt_driver.c
@@ -0,0 +1,88 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/device.h>
+#include<linux/interrupt.h>
+#include <linux/platform_device.h>
+
+unsigned int irq = 15;
+
+static irqreturn_t simple_interrupt_handler (int irq, void *dev)
+{
+	static int count =0;
+	if(count<=5) {
+		printk(KERN_ALERT " Interrupt occured on IRQ %d\n", irq);
+		count++;
+	}
+	return IRQ_NONE;
+		
+}
+
+static int simple_platform_driver_probe (struct platform_device *pdev)
+{
+	int ret;
+	printk(KERN_ALERT " %s\n", __func__);
+	ret = request_irq(irq, &simple_interrupt_handler, IRQF_SHARED, "simple-platform-device" , pdev);
+	if(ret) {
+		printk(KERN_ALERT " Failed in request_irq  %s\n", __func__);
+		return -EIO;
+	}
+	
+	return ret;
+
+}
+
+static int simple_platform_driver_remove (struct platform_device *pdev)
+{
+	printk(KERN_ALERT " %s\n", __func__);
+	free_irq(irq, pdev);
+	return 0;
+}
+
+
+static struct platform_device simple_device = {
+	.name = "simple-platform-device",
+	.id = PLATFORM_DEVID_NONE, 
+};
+
+
+static struct platform_driver simple_driver  = {
+	.probe = simple_platform_driver_probe,
+	.remove = simple_platform_driver_remove,
+	.driver = {
+	    .name = "simple-platform-device",
+	    .owner = THIS_MODULE,
+	},
+}; 
+
+
+static int simple_platform_init(void)
+{
+	printk(KERN_ALERT "welcome in platfrom driver init function\n");
+	
+	/* Register my platform device with the kernel */
+	platform_device_register(&simple_device);
+	
+	/* Register my platfrom driver with kernel. 
+	   We will use this when we are not sure if this device is present in the
+	   system. The driver probe will be called by the OS if the device is
+	   present in the system. */
+
+	return platform_driver_register(&simple_driver);
+	
+}
+
+
+static void simple_platform_exit(void)
+{
+	printk(KERN_ALERT "exit %s\n", __func__);
+	platform_driver_unregister(&simple_driver);
+	platform_device_unregister(&simple_device);
+}
+
+module_init(simple_platform_init);
+module_exit(simple_platform_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Namarta Kohli");
+
+
