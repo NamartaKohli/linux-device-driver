781633c976155479462116277d8bd91bbbb51306
Namarta Kohli <Namarta.Kohli@rsystem.com>
2019-04-26 06:12:58 +0200

    driver code to understand workqueue

--- simple_work_queue_driver.c
+++ simple_work_queue_driver.c
@@ -2,22 +2,23 @@
 #include<linux/kernel.h>
 #include<linux/device.h>
 #include<linux/interrupt.h>
-#include<linux/slab.h>
 #include <linux/platform_device.h>
+#include <linux/workqueue.h>
 
 /*you can use any interrupt line which is shared in your system
   you can get this information from cat /proc/interrupts */
 
-static void my_tasklet_handler(unsigned long flag);
+static void my_work_handler(struct work_struct * work);
 
 unsigned int irq = 15; 
 
-struct tasklet_struct  *my_tasklet;
+DECLARE_WORK(my_work, my_work_handler);
 
-static void my_tasklet_handler(unsigned long flag)
+
+static void my_work_handler(struct work_struct * work)
 {
 	
-	printk(KERN_ALERT " inside %s with data %lu\n", __func__, flag);
+	printk(KERN_ALERT " inside %s \n", __func__);
 	return;
 }
 
@@ -27,7 +28,16 @@ static irqreturn_t simple_interrupt_handler (int irq, void *dev)
 	static int count =0;
 	if(count==0) {
 		printk(KERN_ALERT " Interrupt occured on IRQ %d\n", irq);
-		tasklet_schedule(my_tasklet);
+		/*Now that the work is created, we can schedule it. 
+		  To queue a given work's handler function with the default events worker threads call schedule_work
+		  The work is scheduled immediately and is run as soon as the events worker thread on the current processor wakes up.*/
+		schedule_work(&my_work);
+		/*Sometimes you do not want the work to execute immediately, but instead after some delay.
+		  You can schedule work to execute at a given time in the future:
+		  In this case, the work_struct represented by &my_work will not execute for at least delay timer ticks into the future */
+
+//		schedule_delayed_work(&my_work, delay);
+
 		count++;
 	}
 	return IRQ_NONE;
@@ -37,19 +47,12 @@ static irqreturn_t simple_interrupt_handler (int irq, void *dev)
 static int simple_platform_driver_probe (struct platform_device *pdev)
 {
 	int ret;
-	unsigned long data = 156;
 	printk(KERN_ALERT " %s\n", __func__);
 	ret = request_irq(irq, &simple_interrupt_handler, IRQF_SHARED, "simple-platform-device" , pdev);
 	if(ret) {
 		printk(KERN_ALERT " Failed in request_irq  %s\n", __func__);
 		return -EIO;
 	}
-	/* Init the tasklet bt Dynamic Method */
-	my_tasklet  = kmalloc(sizeof(struct tasklet_struct),GFP_KERNEL);
-	if(my_tasklet == NULL) {
-	    printk(KERN_ALERT"simple_driver : cannot allocate Memory");
-	}
-	tasklet_init(my_tasklet, my_tasklet_handler, data);
 	
 	return ret;
 
@@ -59,8 +62,12 @@ static int simple_platform_driver_remove (struct platform_device *pdev)
 {
 	printk(KERN_ALERT " %s\n", __func__);
 	free_irq(irq, pdev);
-	kfree(my_tasklet);
-	tasklet_kill(my_tasklet);
+	flush_scheduled_work();
+	
+	/*Note that "flush_scheduled_work" function does not cancel any delayed work. Any work that was scheduled via schedule_delayed_work(), 
+          and whose delay is not yet up, is not flushed via flush_scheduled_work(). To cancel delayed work, call: cancel_delayed_work */
+
+//	cancel_delayed_work( &my_work);
 	return 0;
 }
 
